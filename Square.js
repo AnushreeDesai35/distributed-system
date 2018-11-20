bb1aa9d9f49af55a0a88ebfc8ca7da893a7ac62b
Anushree <anushreerdesai@gmail.com>
2018-11-19 20:18:56 -0500

    migrate py to js

---
+++ Square.js
@@ -0,0 +1,26 @@
+// from flask import Flask
+// from flask import request, jsonify
+
+// app = Flask(__name__)
+
+// @app.route("/")
+// def hello():
+//     return "Hello World!"
+
+// @app.route('/arith/add', methods=['GET'])
+// def addTwoNos():
+//     return jsonify(int(request.args['x']) + int(request.args['y']))
+
+// @app.route('/arith/subtract', methods=['GET'])
+// def subTwoNos():
+//     return jsonify(int(request.args['x']) - int(request.args['y']))
+
+// # @app.route('/arith/multiply', methods=['GET'])
+// # def mulTwoNos():
+// #     product = 1
+// #     for i in range(int(request.args['x'])):
+// #         product = addTwoNos(product, int(request.args['y']))
+// #     return product
+
+// # if __name__ == '__main__':
+// app.run()
