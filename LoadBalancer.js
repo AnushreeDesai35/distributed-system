33daaeae54fcfb6aa32751a3a9bf7cd169094829
Divyesh Chitroda <div5yesh@gmail.com>
2018-11-27 22:14:40 -0500

    cache SR
     - implement updateSR to update cached SR copy

--- LoadBalancer.js
+++ LoadBalancer.js
@@ -4,27 +4,48 @@ const fetch = require("node-fetch");
 const PORT = 80;
 const registryServer = ["http://localhost:5005"];
 const app = express();
+let cachedSR;
 
 let current = 0;
 
-let forwardServiceRequest = async function(url, req, res) {
-    let response = await fetch(url);
-    let json = await response.json();
-    let endpoints = json.result;
+let fetchCSR = async function (request, response) {
+    let path = request.path;
+    let serviceName = path.substr(path.lastIndexOf('/'));
+    let dicoveryRequest = registryServer[0] + "/service" + serviceName;
 
-    let serviceUrl = "http://" + endpoints[current] + req.url;
-    current = (current + 1) % endpoints.length;
+    let resp = await fetch(dicoveryRequest);
+    let json = await resp.json();
+    let endpoints = json.result;
+    return endpoints;
+};
+
+let forwardServiceRequest = (request, response) => {
+    let serviceUrl = "http://" + cachedSR[current] + request.url;
+    current = (current + 1) % cachedSR.length;
     console.log(`forwarded: ${serviceUrl}`);
-    res.redirect(serviceUrl);
+    response.redirect(serviceUrl);
 };
 
 let requestHandler = (request, response) => {
     console.log(`Request: ${request.url}`);
-    let path = request.path;
-    let serviceName = path.substr(path.lastIndexOf('/'));
-    let dicoveryRequest = registryServer[0] + "/service" + serviceName;
-    forwardServiceRequest(dicoveryRequest, request, response);
+    if (cachedSR) {
+        forwardServiceRequest(request, response);
+    }
+    else {
+        fetchCSR(request, response).then((data) => {
+            cachedSR = data;
+            forwardServiceRequest(request, response);
+        });
+    }
 };
+
+let updateCSR = function(request, response) {
+    console.log(`Update CSR ${cachedSR}`);
+    let endpoints = response.result;
+    cachedSR = endpoints;
+};
+
+app.get("/updateSR", updateCSR);
 
 app.all('*', requestHandler);
 
