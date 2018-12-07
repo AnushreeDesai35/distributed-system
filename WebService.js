e5de13bf398f1339d43e41b3a51760ea3a832490
Divyesh Chitroda <div5yesh@gmail.com>
2018-12-07 01:08:07 -0500

    - add setup batch file for launching servers
     - add unregister service method

--- WebService.js
+++ WebService.js
@@ -11,21 +11,38 @@ class WebService {
             ip,
             port
         };
-        this.registryURL = "http://127.0.0.1:5005/register/<name>";
+        this.registryEndpoint = "http://127.0.0.1:8081";
         this.app = express();
     }
 
     start(handler) {
         this.app.listen(this.endpoint.port, () => {
             console.log(`${this.name} server listening on port ${this.endpoint.port}...`);
-            this.registerService(this.registryURL);
+            this.registerService(this.registryEndpoint);
         });
         this.app.all("*", handler);
     }
 
-    registerService(registryURL) {
-        let url = registryURL.replace(/<name>/, this.name);
-        fetch(url, {
+    registerService(registryEndpoint) {
+        let registryURL = `${registryEndpoint}/register/${this.name}`;
+        fetch(registryURL, {
+                method: 'POST',
+                body: JSON.stringify(this.endpoint),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(json => {
+                if (json.result) {
+                    // init heartbeat
+                }
+            });
+    }
+
+    unregisterService(registryEndpoint) {
+        let registryURL = `${registryEndpoint}/unregister/${this.name}`;
+        fetch(registryURL, {
                 method: 'POST',
                 body: JSON.stringify(this.endpoint),
                 headers: {
