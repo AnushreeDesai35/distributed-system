4ff3d9d5180824f06c9799221790b63950de75ef
Anushree <anushreerdesai@gmail.com>
2018-12-07 01:09:20 -0500

    Service registry parses xml init

--- ServiceRegistry.js
+++ ServiceRegistry.js
@@ -1,40 +1,68 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5005;
-
+const fs = require('fs');
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const convert = require('xml-js');
 
-serviceMapping = {};
+const wsdlFile = "./AddWSDL.xml";
 
-app.get("/services", (req, res) => {
-    console.log(`Request: ${req.url}`);
-    res.send({
-        result: serviceMapping,
-        message: 'Service endpoint returned successfully'
-    });
-});
-
-app.post("/register/:serviceName", (req, res) => {
-    let serviceName = req.params.serviceName;
-    let serviceRecord = serviceMapping[serviceName];
-    if(serviceRecord){
-        if(serviceRecord.endpoints.indexOf(req.body.endpoint) < 0){
-            serviceRecord.endpoints.push(req.body.endpoint);
+let xmlData = null;
+var filePromise = new Promise(function (resolve, reject) {
+    fs.readFile(wsdlFile, 'utf8', function (err, data) {
+        if (err) {
+            console.log(error);
         }
-    } else{
-        serviceMapping[serviceName] = {endpoints: [req.body.endpoint]};
-    }
-    res.send({
-        result: 1,
-        message: "success"
+        resolve(data);
     });
-    console.log(serviceMapping)
-    // init heartbeat
-    // call LB/updateSR
 });
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}...`);
+filePromise.then((data) => {
+    xmlData = data;
+    var xmlToJson = convert.xml2js(xmlData, {
+        compact: true,
+        spaces: 4
+    });
+    console.log("Hi, here is json: ", xmlToJson['wsdl:description']['wsdl:service']);
+
+    // app.use(bodyParser.json());
+    // app.use(bodyParser.urlencoded({
+    //     extended: true
+    // }));
+
+    // var serviceMapping = {};
+    
+
+    app.get("/services", (req, res) => {
+        console.log(`Request: ${req.url}`);
+        res.send({
+            result: xmlData,
+            message: 'Service endpoint returned successfully'
+        });
+    });
+
+    app.post("/register/:serviceName", (req, res) => {
+        let serviceName = req.params.serviceName;
+        let serviceRecord = serviceMapping[serviceName];
+        if (serviceRecord) {
+            if (serviceRecord.endpoints.indexOf(req.body.endpoint) < 0) {
+                serviceRecord.endpoints.push(req.body.endpoint);
+            }
+        } else {
+            serviceMapping[serviceName] = {
+                endpoints: [req.body.endpoint]
+            };
+        }
+        res.send({
+            result: 1,
+            message: "success"
+        });
+        console.log(serviceMapping)
+        // init heartbeat
+        // call LB/updateSR
+    });
+
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}...`);
+    });
 });
