f8a20eaa56d6162c5ee283ccefe240f2b524081b
Anushree <anushreerdesai@gmail.com>
2018-12-11 19:44:58 -0500

    WSDL services files add

--- ServiceRegistry.js
+++ ServiceRegistry.js
@@ -7,62 +7,60 @@ const convert = require('xml-js');
 
 const wsdlFile = "./AddWSDL.xml";
 
-let xmlData = null;
-var filePromise = new Promise(function (resolve, reject) {
-    fs.readFile(wsdlFile, 'utf8', function (err, data) {
-        if (err) {
-            console.log(error);
-        }
-        resolve(data);
+app.get("/services", (req, res) => {
+    // console.log(`Request: ${req.url}`);
+    res.send({
+        result: xmlData,
+        message: 'Service endpoint returned successfully'
     });
 });
 
-filePromise.then((data) => {
-    xmlData = data;
-    var xmlToJson = convert.xml2js(xmlData, {
-        compact: true,
-        spaces: 4
-    });
-    console.log("Hi, here is json: ", xmlToJson['wsdl:description']['wsdl:service']);
-
-    // app.use(bodyParser.json());
-    // app.use(bodyParser.urlencoded({
-    //     extended: true
-    // }));
-
-    // var serviceMapping = {};
-    
-
-    app.get("/services", (req, res) => {
-        console.log(`Request: ${req.url}`);
-        res.send({
-            result: xmlData,
-            message: 'Service endpoint returned successfully'
-        });
-    });
-
-    app.post("/register/:serviceName", (req, res) => {
-        let serviceName = req.params.serviceName;
-        let serviceRecord = serviceMapping[serviceName];
-        if (serviceRecord) {
-            if (serviceRecord.endpoints.indexOf(req.body.endpoint) < 0) {
-                serviceRecord.endpoints.push(req.body.endpoint);
-            }
-        } else {
-            serviceMapping[serviceName] = {
-                endpoints: [req.body.endpoint]
-            };
+app.post("/register/:serviceName", (req, res) => {
+    let serviceName = req.params.serviceName;
+    let serviceRecord = serviceMapping[serviceName];
+    if (serviceRecord) {
+        if (serviceRecord.endpoints.indexOf(req.body.endpoint) < 0) {
+            serviceRecord.endpoints.push(req.body.endpoint);
         }
-        res.send({
-            result: 1,
-            message: "success"
-        });
-        console.log(serviceMapping)
-        // init heartbeat
-        // call LB/updateSR
+    } else {
+        serviceMapping[serviceName] = {
+            endpoints: [req.body.endpoint]
+        };
+    }
+    res.send({
+        result: 1,
+        message: "success"
     });
+    // console.log(serviceMapping);
+    // init heartbeat
+    // call LB/updateSR
+});
 
-    app.listen(port, () => {
-        console.log(`Server listening on port ${port}...`);
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`);
+});
+
+let fileArray = [
+    'WSDLAdd.xml',
+    'WSDLSubtract.xml',
+    'WSDLMultiply.xml',
+    'WSDLDivide.xml',
+    'WSDLDouble.xml',
+    'WSDLSquare.xml'];
+
+let filesXMLData = {};
+fileArray.forEach((file) => {
+    fs.readFile('./'+file, (error, data) => {
+        if(error){
+            console.log('Error while reading file', error);
+        }
+        else {
+            var jsonData = convert.xml2js(data, {
+                compact: true,
+                spaces: 4
+            });
+            filesXMLData[file] = jsonData;
+            console.log(filesXMLData[file]['wsdl:description']['wsdl:binding']);
+        }
     });
 });
