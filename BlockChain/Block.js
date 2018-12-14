9549fd7d33d394a878671ddff9636dabbfc890d8
Divyesh Chitroda <div5yesh@gmail.com>
2018-12-14 01:34:55 -0500

    implement blockchain ledger and block

---
+++ BlockChain/Block.js
@@ -0,0 +1,34 @@
+const sha = require("sha256");
+
+class Block{
+    constructor(transaction, prevHash){
+        this.nounce = 0;
+        this.prevHash = prevHash;
+        this.timestamp = new Date();
+        this.transaction = JSON.stringify(transaction);
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash(){
+        return sha(this.prevHash + this.transaction + this.timestamp + this.nounce);
+    }
+
+    isValid(){
+        return this.hash == this.calculateHash();
+    }
+
+    mine(work){
+        let target = "0".repeat(work);
+        while(!(this.hash.substr(0, work) == target)){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+    }
+
+    isMined(work){
+        let target = "0".repeat(work);
+        return this.hash.substr(0, work) == target;
+    }
+}
+
+module.exports = Block;
---
+++ BlockChain/Ledger.js
@@ -0,0 +1,41 @@
+const Block = require("./Block");
+
+class Ledger{
+    constructor(work){
+        this.work = work;
+        this.chain = [];
+        let genesis = new Block({}, 0);
+        genesis.mine(work);
+        this.chain.push(genesis);
+    }
+
+    record(transaction){
+        let lastBlock = this.chain[this.chain.length - 1]
+        let block = new Block(transaction, lastBlock.prevHash);
+        block.mine(this.work);
+        this.chain.push(block);
+    }
+
+    isValid(){
+        for(let i=1; i < this.chain.length; i++){
+            let prev = this.chain[i - 1];
+            let curr = this.chain[i];
+
+            if(prev.hash != curr.prevHash){
+                return false;
+            }
+
+            if(!curr.isValid()){
+                return false;
+            }
+
+            if(!curr.isMined(this.work)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+module.exports = Ledger;
